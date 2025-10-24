35f301c244f2fb8219b3fc5c2b334fe0cf15dac9
Stuart-200 <swilson.2009@zohomail.com>
2025-10-24 22:25:30 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
-function validate(e){
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const age = document.getElementById("age").value;
-    const msgBox = document.getElementById("messageBox");
-    
-    let message = "";
+function getHistory() {
+    return document.getElementById("history-value").innerText;
 
-    if(email === ''){
-        message = 'Enter an email';
-        msgBox.style.color = 'red';
-    } else if(password === ''){
-        message = 'Enter a password';
-        msgBox.style.color = 'red';
-    } else if (age === ''){
-        message = 'Enter your age';
-        msgBox.style.color = 'red';
-    } else {
-        message = 'Login Successful';        message = 'Login Successful';
-        msgBox.style.color = 'green';ox.style.color = 'green';
-    }
-    msgBox.innerText = message;
+}
+function printHistory(num) {
+    document.getElementById("history-value").innerText = num;
+
 }
